33eac67efc83fe6ea0dd9944c8dbb078c2995874
Bautista Madruga <bmadruga@wilab.io>
2023-11-01 18:00:59 -0300

    fix child

--- src/app/components/urls-list/urls-list.component.ts
+++ src/app/components/urls-list/urls-list.component.ts
@@ -6,30 +6,14 @@ import { IUrl } from 'src/app/models/i-url';
   templateUrl: './urls-list.component.html',
   styleUrls: ['./urls-list.component.css']
 })
-export class UrlsListComponent {
+export class UrlsListComponent  {
 
   urls: IUrl[] = [];
 
-  _newPage!: IUrl;
-
   constructor() {}
 
-
-  private addUrl(url: IUrl): void {
+  addUrl(url: IUrl): void {
     this.urls.push(url);
-  }
-
-  @Input() set newPage(page: IUrl) {
-    if (page) {
-      this.addUrl(page);
-    }
-  }
-
-  ngOnInit(): void {
-  }
-
-  ngOnDestroy(): void {
-   
   }
 
   deleteUrls(index: number): void {
